b0044355b3779b79451698379b904adc2a58771d
Pedro Simões <pedro@decenomy.net>
2020-12-30 22:58:34 +0000

    fix spork check signature function

--- src/spork.cpp
+++ src/spork.cpp
@@ -182,10 +182,11 @@ bool CSporkManager::CheckSignature(CSporkMessage& spork)
     //note: need to investigate why this is failing
     std::string strMessage = boost::lexical_cast<std::string>(spork.nSporkID) + boost::lexical_cast<std::string>(spork.nValue) + boost::lexical_cast<std::string>(spork.nTimeSigned);
 
-    string strSporkKey = "04fc640bba80713c0666acda4d3ffce670307a55f90b703995c830a1e9110b07244508724b7106395f8336c78d3691ae5ba05abe3840f3a7e18d6b95acdd0de71d";
+    //string strSporkKey = "04fc640bba80713c0666acda4d3ffce670307a55f90b703995c830a1e9110b07244508724b7106395f8336c78d3691ae5ba05abe3840f3a7e18d6b95acdd0de71d";
 
-    //CPubKey pubkeynew(ParseHex(Params().SporkKey()));
-    CPubKey pubkeynew(ParseHex(strSporkKey));
+    CPubKey pubkeynew(ParseHex(Params().SporkKey()));
+    //CPubKey pubkeynew(ParseHex(strSporkKey));
+
     //cout << "CheckSignature: " << pubkeynew.GetHex() << "\n";
     std::string errorMessage = "";
     if (obfuScationSigner.VerifyMessage(pubkeynew, spork.vchSig, strMessage, errorMessage)) {
